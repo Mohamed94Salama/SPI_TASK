726f1635304715ca3fd49bc26839994fbd9ffd96
Mohamed94Salama <70778328+Mohamed94Salama@users.noreply.github.com>
2021-07-27 03:47:43 +0200

    Update main.c

--- TASK_SPI2/APP/main.c
+++ TASK_SPI2/APP/main.c
@@ -13,31 +13,14 @@ int main(void)
     UART_Init();
 	SPI_SlaveInit();
 
-	//UART_SendString(x2);
-	/*if(x2[1]=='O')
-	{
-		DIO_Write(4,HIGH);
-	}*/
-
     while (1) 
     {
 	uint8_t* x2;
+	
 	SPI_SlaveReceiveString(x2);
-	uint8_t r=0;
-	/*while(x2[r] != '\r')
-	{
-		UART_SendData(x2[r]);
-		r++;
-	}*/
-	/*if(x2[1]=='O')
-	{
-		DIO_Write(4,HIGH);
-	}*/
-	UART_SendString(x2);
-	//UART_SendString(x2);
-		//DIO_Write(4,HIGH);
-		//UART_SendString("SALAMA");
 
+	UART_SendString(x2);
+	
     }
 }
 
